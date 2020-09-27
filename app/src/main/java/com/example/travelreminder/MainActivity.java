a75f2a0c27293707003269c01a87a7529821d29d
grnsaifkhan <saifkhan110180@gmail.com>
2020-09-28 00:28:04 +0200

    added edittext and button in user interface.

--- app/src/main/java/com/example/travelreminder/MainActivity.java
+++ app/src/main/java/com/example/travelreminder/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.travelreminder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        Log.e("TAG", "DATABASE CONNECTED" + database);
+
+
     }
 }
