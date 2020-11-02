3671075bb0e87776a7c16eac1332e0caaf67cf00
grnsaifkhan <saifkhan110180@gmail.com>
2020-11-02 01:39:34 +0100

    updating xml file data

--- app/src/main/java/com/example/travelreminder/MapFragment.java
+++ app/src/main/java/com/example/travelreminder/MapFragment.java
@@ -1,6 +1,7 @@
 package com.example.travelreminder;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,13 +9,23 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
-public class MapFragment extends Fragment {
-    @Nullable
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class MapFragment extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap mapApi
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view =  inflater.inflate(R.layout.map_fragment,container,false);
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.map_fragment);
+    }
 
-        return view;
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
