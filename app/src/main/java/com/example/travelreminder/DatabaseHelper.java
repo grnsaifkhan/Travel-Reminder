82800d3f9ab9f1797b45e41448e0fb4fb72ff569
grnsaifkhan <saifkhan110180@gmail.com>
2021-01-11 01:35:16 +0100

    Updated info_save_fragment.xml file. Added new features in InfoSave_fragment.java fragment. Added new edittext in travel_listview_fragment.xml file. added DatabeseHelper.java class for sqlite database

---
+++ app/src/main/java/com/example/travelreminder/DatabaseHelper.java
@@ -0,0 +1,74 @@
+package com.example.travelreminder;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+
+    private static final String TAG = "DatabaseHelper";
+
+    private static final String DATABASE_NAME = "travelreminder.db";
+
+    private static final String TABLE = "travel_table";
+
+    private static final String ID = "ID";
+
+    private static final String TRAVEL_NAME= "TRAVEL_NAME";
+
+    private static final String DESTINATION = "DESTINATION";
+
+    private static final String TRAVEL_DATE= "TRAVEL_DATE";
+
+    private static final String TRAVEL_TIME = "TRAVEL_TIME";
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        String createTable = "CREATE TABLE "+TABLE+" (ID INTEGER PRIMARY KEY AUTOINCREMENT,TRAVEL_NAME TEXT,DESTINATION TEXT,TRAVEL_DATE TEXT,TRAVEL_TIME TEXT)";
+        sqLiteDatabase.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+TABLE);
+        onCreate(sqLiteDatabase);
+    }
+
+    public boolean insertData(String travelName,String destination,String travelDate,String travelTime){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(TRAVEL_NAME,travelName);
+        contentValues.put(DESTINATION, destination);
+        contentValues.put(TRAVEL_DATE,travelDate);
+        contentValues.put(TRAVEL_TIME,travelTime);
+
+        Log.d(TAG,"insertData : Adding "+travelName+" "+destination+" "+travelDate+" "+travelTime+"to "+TABLE);
+
+        long result = sqLiteDatabase.insert(TABLE,null,contentValues);
+
+        if (result == -1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public Cursor getData(){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        String query = "SELECT * FROM "+TABLE;
+        Cursor data = sqLiteDatabase.rawQuery(query, null);
+        return data;
+    }
+}
--- app/src/main/java/com/example/travelreminder/InfoSave_Fragment.java
+++ app/src/main/java/com/example/travelreminder/InfoSave_Fragment.java
@@ -1,20 +1,128 @@
 package com.example.travelreminder;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.Calendar;
+
 public class InfoSave_Fragment extends Fragment {
+
+    DatabaseHelper mDatabaseHelper;
+    Button bt_save;
+    EditText et_travel_name, et_destination, et_travel_date, et_travel_time;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.info_save_fragment, container, false);
 
+        et_travel_name = (EditText) rootView.findViewById(R.id.travel_name);
+        et_destination = (EditText) rootView.findViewById(R.id.destination);
+        et_travel_date = (EditText) rootView.findViewById(R.id.travel_day);
+        et_travel_time = (EditText) rootView.findViewById(R.id.travel_time);
+        bt_save = (Button)rootView.findViewById(R.id.save_button);
+
+        mDatabaseHelper = new DatabaseHelper(getContext());
+
+        insertData();
+        editTextToTimePicker();
+        editTextToDatePicker();
+
+
         return rootView;
+    }
+
+    public void insertData(){
+        bt_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (et_travel_name.length() == 0 || et_destination.length() == 0 ||et_travel_date.length()==0||et_travel_time.length()==0){
+                    warningMessage("Warning!", "Please fill up every text field properly!");
+                }else{
+                    boolean inputData = mDatabaseHelper.insertData(et_travel_name.getText().toString(),et_destination.getText().toString(), et_travel_date.getText().toString(),et_travel_time.getText().toString());
+
+                    if (inputData == true){
+                        warningMessage("Notice : ","Travel info added successfully");
+                        et_travel_name.setText("");
+                        et_destination.setText("");
+                        et_travel_date.setText("");
+                        et_travel_time.setText("");
+                    }
+                    else {
+                        warningMessage("Warning","Data not inserted");
+                    }
+                }
+            }
+        });
+    }
+
+    public void editTextToTimePicker(){
+        et_travel_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar mCurrentTime =  Calendar.getInstance();
+                int hour = mCurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mCurrentTime.get(Calendar.MINUTE);
+
+                TimePickerDialog timePickerDialog;
+                timePickerDialog = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        et_travel_time.setText(selectedHour+":"+selectedMinute);
+                    }
+                },hour,minute,true);
+                timePickerDialog.setTitle("Select Travel time");
+                timePickerDialog.show();
+            }
+        });
+    }
+
+    public void editTextToDatePicker(){
+        et_travel_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        et_travel_date.setText(dayOfMonth+"/"+(month+1)+"/"+year);
+                    }
+                },day,month,year);
+                datePickerDialog.show();
+            }
+        });
+    }
+
+    public void warningMessage(String title,String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setCancelable(false);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
     }
 }
